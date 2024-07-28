a88d4f2f67700234bf67dc0707d4fe0366ec4644
ghtwang <160577673+ghtwang@users.noreply.github.com>
2024-07-28 14:43:10 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,8 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# add cups
+cd lede/package/lean
+git clone https://github.com/obanat/openwrt-printing-packages.git
+
